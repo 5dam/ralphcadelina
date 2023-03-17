ce3d24ea2d5f74ac99729e9d87b2b8cf3dc459b7
5dam <ralphcadelina5@gmail.com>
2023-03-17 15:38:37 +0800

    about page done

---
+++ src/components/Skills.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import HTML from '../assets/html.png'
+
+
+const Skills = () => {
+  return (
+    <div name='skills'>
+        {/* Container */}
+        <div>   
+
+        </div>
+    </div>
+  )
+}
+
+export default Skills
