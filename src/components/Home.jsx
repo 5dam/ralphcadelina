8c4bddd76de1db324f3dc968a74c912e5bbe5424
5dam <ralphcadelina5@gmail.com>
2023-03-17 22:06:00 +0800

    test

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
             <p className='text-pink-600'>Hi, my name is </p>
             <h1 className='text-4xl sm:text-7xl font-bold dark:text-white'>Ralph Cadeliña</h1>
             <h2 className='text-4xl sm:text-7xl font bold dark:text-white'>I'm a Frontend Developer.</h2>
-            <p className='dark:text-white py-4 max-w-[700px]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil debitis dignissimos alias excepturi, dolorum exercitationem hic nulla placeat itaque ipsum!</p>
+            <p className='dark:text-white py-4 max-w-[700px]'>A passionate Front-end React Developer based in Surigao City, Philippines.</p>
             <div>
                 <button className='dark:text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600 hover:text-white'>View Work<HiArrowNarrowRight className='ml-3' /></button>
             </div>
@@ -20,3 +20,5 @@ const Home = () => {
 }
 
 export default Home
+
+ 
