b7526415073d74d864465c5f6128558893dbdc62
5dam <ralphcadelina5@gmail.com>
2023-06-29 19:05:19 +0800

    update

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import WorkImage1 from '../assets/works/mkdph-front.png'
-import WorkImage4 from '../assets/works/smithfields.png'
 import WorkImage5 from '../assets/works/portfoliopic.png'
 
 const Project = () => {
@@ -17,26 +16,6 @@ const Project = () => {
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8">
 
         {/* Grid item */}
-          <div
-            style={{ backgroundImage: `url(${WorkImage4})` }}
-            className="shadow-lg shadow-[#040c16] dark:shadow-white group container rounded-md 
-                      flex justify-center text-center items-center mx-auto content-div">
-            
-            {/* Hover effects */}
-            <div className="opacity-0 group-hover:opacity-100 ">
-              <span className='text-xl font-bold text-white tracking-wider'>
-                 A Fullstack web application intended for mobile users. Built using MERN stack.
-              </span>
-              <div className='pt-8 text-center'>
-                <a href="https://robinhoodai.shop/" target="_blank" rel="noopener noreferrer">
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white hover:scale-110 dark:text-black'>Demo</button>
-                </a>
-                <a href="https://github.com/5dam/robinhoodx" target="_blank" rel="noopener noreferrer">
-                  <button className='text-center rounded-lg px-4 py-3 m-2 bg-white hover:scale-110 dark:text-black'>Code</button>
-                </a>
-              </div>
-            </div>
-          </div>
 
           <div
             style={{ backgroundImage: `url(${WorkImage5})` }}
